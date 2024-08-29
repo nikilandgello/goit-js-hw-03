7651e4d2ba16758fcb88f41bc99c30dc428c2391
nikilandgello <nikoscha22012004@gmail.com>
2024-08-29 22:37:48 +0300

    arrays

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+    title = title.toLowerCase();
+    return title.split(" ").join("-");
+};
+
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
